a739f806c7b7085ccfd6e2820211e990477ad8cc
yubin1121 <kid__ing@naver.com>
2025-05-18 10:39:13 +0900

    multi module setting

---
+++ module-api/src/main/java/kr/co/module/api/ModuleApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.module.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
